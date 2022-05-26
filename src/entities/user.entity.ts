70bec23fd957dfb31edd668b5bd6b9cebf5f0dde
alsrlqor1007 <skyup9393@gmail.com>
2022-05-26 14:26:22 +0900

    Move directory

---
+++ src/entities/user.entity.ts
@@ -0,0 +1,26 @@
+import { Post } from "src/entities/post.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, OneToMany, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity('User')
+export class User {
+    @PrimaryGeneratedColumn()
+    user_id: number;
+
+    @Column({ type: 'varchar', unique: true })
+    nickname: string;
+
+    @Column({ type: 'varchar' })
+    password: string;
+
+    @CreateDateColumn({ type: 'timestamp' })
+    create_date_time: Date;
+
+    @UpdateDateColumn({ type: 'timestamp' })
+    update_date_time: Date;
+
+    @DeleteDateColumn({ type: 'timestamp', nullable: true })
+    deleted_date_time: Date;
+
+    @OneToMany(() => Post, post => post.writer_id, { cascade: true })
+    posts: Post[];
+}
