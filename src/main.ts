6b94de5b8eba3605744dc29eaca5b135ed311faf
mingi <skyup9393@gmail.com>
2022-08-19 16:57:09 +0900

    Delete unused import

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,17 @@ import { Logger, ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
-import { ConfigService } from '@nestjs/config';
 import * as dotenv from 'dotenv';
 import * as path from 'path';
 
 dotenv.config({
   path: path.resolve(
-    (process.env.NODE_ENV === 'prod') ? '.prod.env'
-      : (process.env.NODE_ENV === 'dev') ? '.dev.env' : '.test.env'
-  )
+    process.env.NODE_ENV === 'prod'
+      ? '.dev.env'
+      : process.env.NODE_ENV === 'dev'
+      ? '.dev.env'
+      : '.test.env',
+  ),
 });
 
 async function bootstrap() {
@@ -22,23 +24,23 @@ async function bootstrap() {
     .setTitle('bulletin_board_api')
     .setDescription('Board API Document by Mingi')
     .setVersion('1.0')
-    .build()
+    .build();
 
   const document = SwaggerModule.createDocument(app, config);
-    
+
   SwaggerModule.setup('api', app, document);
 
   app.useGlobalPipes(
     new ValidationPipe({
-      whitelist : true, 
-      forbidNonWhitelisted : true,
-      transform : true
-    })
-  )
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
 
   // const serverConfig = app.get(ConfigService);
   // const port = serverConfig.get<string>('server.port');
-  
+
   await app.listen(process.env.SERVER_PORT);
   Logger.log(`Board API server running on ${process.env.SERVER_PORT}!`);
 }
